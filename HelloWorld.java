034ff95de99cee0041b37fd4e213fd756e88cd43
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-03-18 12:19:05 +0700

    add Helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class Hello World {
+public class HelloWorld {
     public static void main(String[] args){
         System.out.print("Hello World");
         System.out.println("Hello Chonburi");
