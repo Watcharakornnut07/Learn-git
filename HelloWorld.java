b3a1a8672e3a664ce292240f82c59679ed028c65
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-03-18 10:28:35 +0700

    add java-edit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ public class Hello World {
     public static void main(String[] args){
         System.out.print("Hello World");
         System.out.println("Hello Chonburi");
+        System.out.println("Hello Chonburi 2");
+        System.out.println("Hello Sattahip");
+
+        
     }
 }
