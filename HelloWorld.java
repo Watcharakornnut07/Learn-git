545a47793787a18e382c94c533be810f53f96d33
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-04-02 14:50:29 +0700

    add test.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
         System.out.println("Hello Friday [TGIF]");
         System.out.println("Hello Songkran day");
         System.out.println("Hello songkran II");
+        System.out.println("test");
         
 
         
