9e57f1031a3e21a82703272051967b440e9433d4
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-05-16 09:05:04 +0700

    Hello New World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,39 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){    
 
-        System.out.print("Hello World");
-        System.out.println("Hello Chonburi");
-        System.out.println("Hello Chonburi 2");
-        System.out.println("Hello Sattahip");
-        System.out.println("Hello JAVA Thailand");
-        System.out.println("Hello Today");
-        System.out.println("Hello");
-        System.out.println("Hello Test");
-        System.out.println("TGIF");
-        System.out.println("Hello Monday");
-        System.out.println("Hello Sawasdee krub");
-        System.out.println("Hello Friday [TGIF]");
-        System.out.println("Hello Songkran day");
-        System.out.println("Hello songkran II");
-        System.out.println("test");
-        System.out.println("Hello it me");
-        System.out.println("Hello Hello Test");
-        System.err.println("hello april 05 2024");
-        System.out.println("Hello Hello 09");
-        System.out.println("Hello it'is world");
-        System.out.println("Hello test Hello thuseday");
-        System.out.println("Hello 12th Month");
-        System.out.println("Hello open world");
-        System.out.println("Hello Monday");
-        System.out.println("Hello tuseday");
-        System.out.println("Hello Wednesday");
-        System.out.println("Hello work");
-        System.out.println("Hello MOnday/29");
-        System.out.println("Hello it me");
-        System.out.println("Hello Siam");
-        System.out.println("Hello monday 05");
-        System.out.println("Hello tuesday");
-        System.out.println("Hello freeday");
+        System.out.print("Hello World");        
         
     }
 }
