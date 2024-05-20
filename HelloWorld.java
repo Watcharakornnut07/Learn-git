f00f46a55bc6a67f7a6cce90dd0e2cecadc69e8b
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-05-20 09:32:21 +0700

    Hello world II

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){    
-
-        System.out.print("Hello World");        
+        System.out.println("Hello World");
+        System.out.println("Hello World II");        
         
     }
 }
