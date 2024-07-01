612fe7323d78d31ac4f4b1f3c36b350305ed9667
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-07-01 09:34:38 +0700

    Monday

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,31 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){    
         System.out.println("Hello World");
-        System.out.println("Hello World II"); 
-        System.out.println("Hello World III");  
-        System.out.println("Hello World IIII");    
-        System.out.println("Hello Thuesday");
-        System.out.println("Hello siam");
-        System.out.println("Hello World II");
-        System.out.println("Hello Test");
-        System.out.println("Hello Tuesday 28th");
-        System.out.println("Hello It's me");
-        System.out.println("Hello Thuesday");
-        System.out.println("Hello TGIF");
-        System.out.println("Hello 06");
-        System.out.println("Hello 06/06/2024");
-        System.out.println("Hello Monday");
-        System.out.println("Hello UAT iPortman");
-        System.out.println("Hello Hello TGIF");
-        System.out.println("Hello monday II");
-        System.out.println("Hello Tuesday");
-        System.out.println("Hello Thuesday II ");
-        System.out.println("TGIF Hello Friday"); 
-        System.out.println("Hello Monday 24");
-        System.out.println("Hello Notion");    
-        System.out.println("UAT iPortmam 3rd.");
-        System.out.println("Hello Thursday 27");
-
-        
+              
     }
 }
