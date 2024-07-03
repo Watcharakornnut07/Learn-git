e5021f580bb2f22cc9ab8cc56b847d385f1f291d
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-07-03 17:24:35 +0700

    Hello Monday IIIIII

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
 public class HelloWorld {
-    public static void main(String[] args){    
+    public static void main(String[] args){   
+        
         System.out.println("Hello World");
         System.out.println("Hello World II");
         System.out.println("Hello World III");
         System.out.println("Hello World IIII");
+        
               
     }
 }
