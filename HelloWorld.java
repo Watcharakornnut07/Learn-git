5a78bd59ebb07377e42c67f67617e59312ec8bd9
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-08-02 16:45:57 +0700

    Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,31 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){   
         
-        System.out.println("Hello World");
-        System.out.println("Hello World II");
-        System.out.println("Hello World III");
-        System.out.println("Hello World IIII");
-        System.out.println("Hello World V");
-        System.err.println("Hello monday IVIV");
-        System.out.println("Hello It'me");
-        System.out.println("Hello Word IV SIAM");
-        System.out.println("Hello World TGIF");
-        System.out.println("Sawasdee krub");
-        System.out.println("Hello Monday 15th");
-        System.out.println("Hello Tuesday 16th");
-        System.out.println("Hello III Day");
-        System.out.println("Hello Thuesday");
-        System.out.println("TGIF");
-        System.out.println("Hello Monday 22th");
-        System.out.println("Hello fist play football of the month"); 
-        System.out.println("Hello Tuesday");
-        System.out.println("Hello Weseday xxx , Hello");
-        System.out.println("Hello Thuesday");
-        System.out.println("Hello Friday");
-        System.out.println("Hello world");
-        System.out.println("Watcharakornnut");
-        System.out.println("Hello It me watcharakornnut");
-        System.out.println("Thuesday Watcharakorn job interview");
+        System.out.println("Hello World"); 
 
               
     }
