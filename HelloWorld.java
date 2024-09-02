8f2118574a02976b098ce0e8a172365028ea09cb
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-09-02 09:56:42 +0700

    Hello Monday

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,20 +2,7 @@ public class HelloWorld {
     public static void main(String[] args){   
         
         System.out.println("Hello World"); 
-        System.out.println("Hello Monday it's Monday");
-        System.out.println("Thuesday");
-        System.out.println("Hello Weseday = Good day");
-        System.out.println("Hello Friday TGIF");
-        System.out.println("Today is monday");
-        System.out.println("Thuseday is 15");
-        System.out.println("Hello Friday TGIF");
-        System.out.println("Hello Sunday ");
         System.out.println("Hello Monday ");
-        System.out.println("Hello Weseday testing");
-        System.out.println("Hello TGIF Firday");
-        System.out.println("Hello Saturday");
-        System.out.println("Hello Monday");
-        System.out.println("Hello Weseday Woohoo ");
               
     }
 }
