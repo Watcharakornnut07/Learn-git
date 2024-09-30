76d2d5d1ae166e705db9f2164843b9c77762347d
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-09-30 10:44:19 +0700

    Hello it me

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,22 +2,8 @@ public class HelloWorld {
     public static void main(String[] args){   
         
         System.out.println("Hello World"); 
-        System.out.println("Hello Monday ");
-        System.out.println("Hello Tuesday ");
-        System.out.println("Project Go-Live ");
-        System.out.println("Hello Weseday  Testing ");
-        System.out.println("Hello It me ");
-        System.out.println("Hello Saturday");
-        System.out.println("Hello Monday");
-        System.out.println("Hello Tuseday watcharakorn D.");
-        System.out.println("Hello Friday TGIF");
-        System.out.println("Hello it me");
-        System.out.println("hello Monday ");
-        System.out.println("Hello Tuesday");
-        System.out.println("Hello Weasday ");
-        System.out.println("Hello Thuesday Sep.");
-        System.out.println("Hello it's me  ");
-        System.out.println("Hello TGIF");
+        System.out.println("Hello Monday it's me");      
+
               
     }
 }
