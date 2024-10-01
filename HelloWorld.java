8d970639209752258013de7ad3e259da16fdc9ca
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-10-01 09:31:20 +0700

    Hello Tuesday

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
     public static void main(String[] args){   
         
         System.out.println("Hello World"); 
-        System.out.println("Hello Monday it's me");      
+        System.out.println("Hello Monday it's me"); 
+        System.out.println("Hello Tuesday ");     
 
               
     }
