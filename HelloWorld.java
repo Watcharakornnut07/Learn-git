d87a0b8d82c6d43bdd0cd9e84bafbd448df2bd55
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-10-30 11:51:54 +0700

    origin

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
         System.out.println("Hello World"); 
         System.out.println("Hello Tuesday it is a book. ");
         System.out.println("Hello it's me");
+        System.out.println("Hello it Friday");
 
               
     }
