7b8ceb6b45f630dd9c9858cdc832164150bc2c02
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-11-20 16:24:46 +0700

    Hello it's me

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,22 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){   
         
         System.out.println("Hello World"); 
-        System.out.println("Hello Tuesday it is a book. ");
-        System.out.println("Hello it's me");
-        System.out.println("Hello it Friday");
-        System.out.println("Hello TGIF");
-        System.out.println("Hello It's me monday");
-        System.out.println("Hello It's Tuesday");
-        System.out.println("Hello Friday ");
-        System.out.println("Hello It Saturday ");
-        System.out.println("Hello Monday ");
-        System.out.println("Hello Weseday ");
-        System.out.println("Hello Thuesday ");
-        System.out.println("Hello TGIF");
-        System.out.println("Hello Monday");
-        System.out.println("Hello iT's me");
-        System.out.println("Hello Tuesday ");
-        System.out.println("Weaseday it's me");
               
     }
 }
