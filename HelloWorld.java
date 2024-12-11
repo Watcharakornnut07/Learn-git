d9cc3add0739373d037a6b8eaaeecd2e7568b56a
Watcharakorn Deachaboon <dwatcharakorn@ABSS.CO.TH>
2024-12-11 11:23:56 +0700

    Hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){   
         
-        System.out.println("Hello World"); 
-        System.out.println("Hello world II ");
-        System.out.println("Hello world III ");
-        System.out.println("Hello it's TGIF");
-        System.out.println("Hello it'me Hello world ");
-        System.out.println("Hello Wednesday Hello World ");
-        System.out.println("Thuesday : it's me ");
-        System.out.println("Hello it me TGIF ");
-        System.out.println("Hello Monday ");
-        System.out.println("Hello it me Tuesday ");
-        System.out.println("Hello it wednesday : Hello it's me : ");
-        System.out.println("Hello it's me : ");
-        
+        System.out.println("Hello World");       
               
     }
 }
